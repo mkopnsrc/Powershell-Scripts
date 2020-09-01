80543d43f58d714e6b2a43c6b024db41ea4d5dfb
Mahesh Kakadia <mkakadia@clearpoolgroup.com>
2020-09-01 17:23:38 -0400

    new script Get-NetAdapterNetbiosOptions.ps1

---
+++ Functions/Get-NetAdapterNetbiosOptions.ps1
@@ -0,0 +1,71 @@
+﻿Function Get-NetAdapterNetbiosOptions() {
+<#
+.SYNOPSIS
+None
+ 
+.DESCRIPTION
+None
+ 
+.EXAMPLE
+Get-NetAdapterNetbiosOptions
+Get-NetAdapterNetbiosOptions -Filter (Enabled,Disabled,All)
+ 
+.INPUTS
+None
+ 
+.OUTPUTS
+Interface Name                           Index TcpipNetbiosOptions
+--------------                           ----- -------------------
+Intel(R) Ethernet Connection (7) I219-LM 1     Disabled           
+Hyper-V Virtual Ethernet Adapter         2     Default
+ 
+.NOTES
+None
+#>
+
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("Enabled","Disabled","All")]
+    [string] $Filter
+  )
+  
+  $HT = @()
+
+  $Lines = @(wmic nicconfig get caption,index,TcpipNetbiosOptions).foreach({$(($_) -replace('\s{2,}',','))}) # REPLACES ALL OCCURENCES OF 2 OR MORE SPACES IN A ROW WITH A SINGLE COMMA
+  $Lines=$Lines.Where({$_ -ne ''}) #Remove empty spaces
+
+  $header=$($Lines[0].split(',').trim())  # EXTRACTS THE FIRST ROW FOR ITS HEADER LINE
+  $header=$header.Where({$_ -ne ''})
+  $header[0]="Interface Name" # overide name; defualt-> Caption
+
+  Write-Debug "Header: $header"
+
+  for($i=1;$i -lt $($Lines.Count);$i++){ # NOTE $i=1 TO SKIP THE HEADER LINE
+
+    $Res = "" | Select-Object $header # CREATES AN EMPTY PSCUSTOMOBJECT WITH PRE DEFINED FIELDS
+    $Line = $($Lines[$i].split(',')).foreach({ $_.trim().trim('>') }).Where({$_ -ne ''}) # SPLITS AND THEN TRIMS ANOMALIES THEN REMOVES EMPTY FIELDS
+    Write-Debug "$Line"
+
+    for($x=0;$x -lt $($Line.count);$x++){
+          if($x -eq 0){ $Line[$x]=$Line[$x] -replace "\[\d{8}\] ",'' }
+          if($x -eq 2){
+              if ($Line[$x] -eq 0){ $Line[$x] = 'Default' }
+              elseif ($Line[$x] -eq 1){ $Line[$x] = 'Enabled' }
+              elseif ($Line[$x] -eq 2){ $Line[$x] = 'Disabled' }
+          }
+      $Res.$($header[$x]) = $Line[$x] # DYNAMICALLY ADDS DATA TO $Res
+    }
+    $HT += $Res # APPENDS THE LINE OF DATA AS PSCUSTOMOBJECT TO AN ARRAY
+
+    Remove-Variable Res # DESTROYS THE LINE OF DATA BY REMOVING THE VARIABLE
+  }
+
+  Switch($Filter) {
+    Enabled { $HT | ? { ($_.TcpipNetbiosOptions -match 'Enabled|Default' ) } }
+    Disabled { $HT | ? { $_.TcpipNetbiosOptions -eq 'Disabled' } }
+    All { $HT }
+    Default { $HT | ? { $_.TcpipNetbiosOptions -ne $null } }
+  }
+  Remove-Variable HT
+}
