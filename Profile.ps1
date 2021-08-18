a1f18ee9d5b56895f22f7ee56c53e9ff04dae0a6
Mahesh K <kakadiam@gmail.com>
2021-08-18 19:42:15 -0400

    Updated profile.ps1 variable bug fix

--- Profile.ps1
+++ Profile.ps1
@@ -1,8 +1,9 @@
-$SPath = split-path -parent $MyInvocation.MyCommand.Definition
-$PSRepoFolder ="$SPath\Functions"
+$SPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$PSRepoFolder = "$SPath\Functions"
 
-Import-Module $SPath\IsAdmin.ps1
-Foreach ($module in Get-Childitem $PSRepoFolder -Name -Filter "*.ps1" -Recurse -Force -WarningAction SilentlyContinue )
-{
-  Import-Module $PSRepoFolder\$module
+Import-Module "$PSRepoFolder\IsLocalAdmin.psm1"
+Import-Module "$PSRepoFolder\IsMemberOf.psm1"
+
+Foreach ($PSCustomModule in Get-Childitem $PSRepoFolder -Name -Filter "*.ps1" -Recurse -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue) {
+  Import-Module "$PSRepoFolder\$PSCustomModule"
 }
